f0d65466ceb387ef5df124015bd91a2af2c6b1dd
majed5sami-sys <majed5sami@gmail.com>
2025-09-17 22:11:07 +0300

    Update mybot_ready.py

--- mybot_ready.py
+++ mybot_ready.py
@@ -1,39 +1,32 @@
-
 import discord
 from discord.ext import commands
-from flask import Flask
-import threading
-
-# -------- keep_alive --------
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running 24/7!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
-# ----------------------------
 
 intents = discord.Intents.default()
-intents.members = True
+intents.members = True  # عشان يتابع الأعضاء الجدد
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot is ready! Logged in as {bot.user}")
+    print(f"✅ تم تسجيل الدخول باسم: {bot.user}")
 
 @bot.event
 async def on_member_join(member):
-    channel = discord.utils.get(member.guild.text_channels, name="✨〢𝐖𝐞𝐥𝐜𝐨𝐦𝐞")
+    channel = discord.utils.get(member.guild.text_channels, name="✨〢𝐖𝐞𝐥𝐜𝐨𝐦𝐞")  # غير اسم الروم إذا تبينه
     if channel:
-        await channel.send(f"أهلاً وسهلاً بك يا {member.mention} 💖")
+        embed = discord.Embed(
+            title="👑 WELCOME TO 7F 👑",
+            description=(
+                f" {member.mention}\n\n"
+                "https://canary.discord.com/channels/1394091822661373972/1415074648478978169\n"
+                "🎉 **استمتع مع الشباب**\n"
+            ),
+            color=0xffcc00
+        )
+        embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+        embed.set_image(url="https://i.ibb.co/cXNF0Kwg/2025-09-13-134516.png")
+        embed.set_footer(text="7F Server")
+        await channel.send(embed=embed)
 
-# ---- شغل keep_alive ثم البوت ----
-keep_alive()
-bot.run("توكن_البوت_هنا")
+# حطي التوكن بين علامتي التنصيص هنا
+bot.run("MTQxNjg1MDk4OTg2NDE5MDAzNQ.Gk7tz4.gaMAsQY0xwBLOA4MKzYO6gL-OGLXJlMwaGlhd4")
